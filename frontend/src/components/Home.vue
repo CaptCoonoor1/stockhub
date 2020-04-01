329eb2209d03f26e277c436e0670fb653913c378
Mujahid Jaffer <mujahidrj@gmail.com>
2020-04-01 16:42:45 -0400

    Updated Home Page
    Descriptions

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,53 +1,50 @@
 <template>
-  <div class="home">
-    <section class="hero">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">
-         {{heading}}
-      </h1>
-      <div class="is-two-thirds column is paddingless">
-        <h2 class="subtitle is-4">
-          {{subheading}}
-        </h2>
+<div class="home">
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">{{heading}}</h1>
+        <div class="is-two-thirds column is paddingless">
+          <h2 class="subtitle is-4">{{subheading}}</h2>
+        </div>
+        <div class="buttons">
+          <router-link to="/login" class="button is-medium is-primary is-rounded">Join now</router-link>
+        </div>
       </div>
-      <div class="buttons">
-        <router-link to="/login" class="button is-medium is-primary is-rounded">Join now</router-link>
-      </div>
-      </a>
-    </div>
-  </div>
-</section>
-
-<body>
-  <section class="section">
-    <div class="container">
-      <h1 class="title">What is The StockHub?</h1>
-      <h2 class="subtitle">
-        Is unmatched, with proven increases in student attendance, engagement and class participation, and improved academic performance and financial behavior.
-      </h2>
-
-      <h1 class="title">Student mobile apps</h1>
-      <h2 class="subtitle">
-        Our mobile app works in conjunction with student team portfolios, engaging students in meaningful second-screen experiences.
-      </h2>
     </div>
   </section>
-</body>
-  </div>
+
+  <body>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">What is StockSim?</h1>
+        <h2 class="subtitle">
+          Our website allows students to build their own virtual stock portfolio, which is linked up to stock prices in real time and allows students to
+          see how their portfolio changes over time. It is unmatched, and has shown an upwards trend in student attendance, engagement and class
+          participation as well as improved academic performance and financial behavior.
+        </h2>
+
+        <h1 class="title">Mobile App</h1>
+        <h2
+          class="subtitle"
+        >Our mobile app works in tandem with student team portfolios, engaging students in a meaningful second-screen experience.</h2>
+      </div>
+    </section>
+  </body>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  data(){
+  name: "home",
+  data() {
     return {
-      heading: 'Stock Market Game',
-      subheading: 'Start with $100,000 in virtual cash and put your trading skills to the test!'
-    }
+      heading: "Stock Market Game",
+      subheading:
+        "Start with $10,000 in virtual cash and put your trading skills to the test!"
+    };
   }
-
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -61,8 +58,8 @@ export default {
 
   .title
     +desktop
-      font-size: 4rem
-      margin-top: 4rem
+    font-size: 4rem
+    margin-top: 4rem
 .container
   margin-left: 4rem
 
