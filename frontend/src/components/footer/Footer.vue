ab913706dc1b45c2c1c8c8e675fa9052ef5930a4
Mujahid Jaffer <mujahidrj@gmail.com>
2020-04-02 14:05:55 -0400

    updated group number

--- frontend/src/components/footer/Footer.vue
+++ frontend/src/components/footer/Footer.vue
@@ -1,19 +1,20 @@
 <template>
-	<footer class="footer">
-		<div class="container">
-			<div class="content">
-				<div class="content has-text-centered">
-					<p>
-						<strong>StockSim</strong> by Group 18 from
-						<a href="https://catalog.ucf.edu/preview_course_nopop.php?catoid=3&coid=6690"
-							>UCF COP4331</a
-						>
-					</p>
-					<p>Copyrights @ Group18 Spring 2020</p>
-				</div>
-			</div>
-		</div>
-	</footer></template
+  <footer class="footer">
+    <div class="container">
+      <div class="content">
+        <div class="content has-text-centered">
+          <p>
+            <strong>StockSim</strong> by Group 19 from
+            <a
+              href="https://catalog.ucf.edu/preview_course_nopop.php?catoid=3&coid=6690"
+            >UCF COP4331</a>
+          </p>
+          <p>Copyrights @ Group19 Spring 2020</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template
 >
 
 <script>
