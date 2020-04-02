9d31f81817137b88791e7b44ad41a338de3cff3e
Mujahid Jaffer <mujahidrj@gmail.com>
2020-04-01 22:28:51 -0400

    First Commit of Stocks Page

--- frontend/src/components/stocks/Stocks.vue
+++ frontend/src/components/stocks/Stocks.vue
@@ -1,12 +1,26 @@
 <template>
 	<div class="stocks">
-		<p>Stocks</p>
+		<app-stock v-for="stock in stocks" :stock="stock" :key="stock.id"></app-stock>
 	</div>
 </template>
 
 <script>
+import Stock from './Stock.vue';
 export default {
 	name: 'Stocks',
+	data() {
+		return {
+			stocks: [
+				{ id: 1, name: 'BMW', price: 110 },
+				{ id: 2, name: 'Google', price: 200 },
+				{ id: 3, name: 'Apple', price: 250 },
+				{ id: 4, name: 'Twitter', price: 8 },
+			],
+		};
+	},
+	components: {
+		appStock: Stock,
+	},
 };
 </script>
 
