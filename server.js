77961a1d55c0a763a27a2d009e4cc4700ccebc20
Nidiyan <nidiyan@gmail.com>
2020-04-07 14:42:43 -0400

    server.js change

--- server.js
+++ server.js
@@ -32,12 +32,11 @@ require('./config/passport')(passport);
 
 // Set headers, etc..
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin: *');
 	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+		'Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization',
 	);
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+	res.setHeader('Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS');
 	next();
 });
 
@@ -56,6 +55,6 @@ mongoose
 	.then(() => console.log('Connected to MongoDB...'))
 	.catch((err) => console.log(err));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`listening on port: ${port}`));
