7c38b3f2b03e25afdbcf277ef5e90793ddc6613a
Nidiyan <nidiyan@gmail.com>
2020-04-17 19:48:58 -0400

    axios baseurl changed

--- frontend/src/axios-auth.js
+++ frontend/src/axios-auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:5000'
+	baseURL: 'http://stock-sim-nid.herokuapp.com'
 });
 
 //instance.defaults.headers.common['SOMETHING'] = 'something'
