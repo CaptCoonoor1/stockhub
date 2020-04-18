d92ed8e786d2e3b55951d6949e4d38b2a621050b
Mujahid Jaffer <mujahidrj@gmail.com>
2020-04-18 18:03:45 -0400

    Revert "money is formatted correctly with dollar sign and commas in appropriate places"
    This reverts commit 89a645d83eb4e72b28a574f874b80ad923b3a3b1.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,12 +12,7 @@ Vue.use(VueToastify, {
 });
 
 Vue.filter('currency', (value) => {
-	let formatter = new Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency: 'USD',
-		minimumFractionDigits: 2,
-	});
-	return formatter.format(value);
+	return '$' + value.toLocaleString();
 });
 
 axios.defaults.baseURL = 'http://localhost:5000';
