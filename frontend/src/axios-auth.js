9e1ccab4ba4f198f1775045ae24c7ad2f5db59f2
Nidiyan <nidiyan@gmail.com>
2020-04-19 11:58:34 -0400

    axios base url fix

--- frontend/src/axios-auth.js
+++ frontend/src/axios-auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: ''//process.env.baseURL || 'http://localhost:5000'
+	baseURL: process.env.NODE_ENV === 'production' ? "" : "http://localhost:5000"
 });
 
 //instance.defaults.headers.common['SOMETHING'] = 'something'
