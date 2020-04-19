98dd57ed9983de36f5f53767f6d749c3ccf613eb
Nidiyan <nidiyan@gmail.com>
2020-04-19 10:59:32 -0400

    axios base url

--- frontend/src/axios-auth.js
+++ frontend/src/axios-auth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const port = process.env.PORT;
 
 const instance = axios.create({
-	baseURL: 'http://localhost:5000'
+	baseURL: process.env.baseURL || 'http://localhost:5000'
 });
 
 //instance.defaults.headers.common['SOMETHING'] = 'something'
